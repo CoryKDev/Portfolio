20c1239f27f9044b8b6f4e658080b38f4f052f24
CoryKDev <corykeelan7@gmail.com>
2021-03-26 15:10:08 -0700

    initial commit and workflow set up for portfolio site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Components here ▼
+
+function App() {
+  return (
+    <div className="App">
+     <h2>hi</h2>
+    </div>
+  );
+}
+
+export default App;
