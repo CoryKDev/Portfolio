a8239b14cdc63a8794fc6c19693b5ab0e6c55a58
CoryKDev <corykeelan7@gmail.com>
2021-03-26 23:30:55 -0700

    background imaged added, contact button added (no functionality yet), and installed npm typed package for typing effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
-     <div className="header-wrapper">
-     <div className="main-info">
-     <h1>Front End Webdeveloper and Web Designer</h1>
-     </div>
-     </div>
+
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Front End Web Developer and Web Designer</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["Web Design",
+                     "Front End Web Development",
+                      "UX/UI"]}
+                    typeSpeed={40}
+                    backSpeed={35}
+                    loop
+                />
+
+                <a href="#" className="btn-main-offer">Contact Me</a>
+            </div>
+        </div>
+
     )
 }
 
