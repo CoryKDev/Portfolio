6611d4abe27c1b43d1a0fad54eab009edf0262e4
CoryKDev <corykeelan7@gmail.com>
2021-03-26 21:52:17 -0700

    Header draft 1 may change in the future

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Components here ▼
+import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-     <h2>hi</h2>
+      <Navbar />
+      <Header />
     </div>
   );
 }
