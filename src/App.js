e8cfaa12a8cbb6fc3ccdd96096e024e540afb63c
CoryKDev <corykeelan7@gmail.com>
2021-03-27 02:44:11 -0700

    added router and react router dom to change pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Particles from 'react-particles-js';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 //import Components here ▼
 import Navbar from './components/Navbar'
 import Header from './components/Header'
@@ -9,29 +11,32 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-    <Particles 
-    className="particles-canvas"
-    params={{
-      particles: {
-        number:{
-        value: 30,
-        density:{
-          enable: true,
-          value_area: 900
-         }
-        },
-        shape: {
-          type: "star",
-          stroke: {
-            width: 6,
-            color: "2cd7dd"
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color: "2cd7dd"
+              }
+            }
           }
-        }
-      }
-    }}
-    /> 
-    <Navbar />
-      <Header />
+        }}
+      />
+      <Navbar />
+
+      <Switch>
+      <Route path="/" component={Header} />
+      </Switch>
     </div>
   );
 }
